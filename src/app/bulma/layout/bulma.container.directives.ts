5a0f923102a6dec51059d85c572a92c09f89301c
8uddishh <senthil.baliah@gmail.com>
2018-07-23 23:11:23 -0500

    fix settimeout using nginit

--- src/app/bulma/layout/bulma.container.directives.ts
+++ src/app/bulma/layout/bulma.container.directives.ts
@@ -1,36 +1,34 @@
-import { Directive, ElementRef, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer, OnInit } from '@angular/core';
 
 @Directive({
     selector: 'bulma-container[fluid]',
 })
-export class BulmaContainerFluidDirective {
-    constructor(private el: ElementRef) {
-        setTimeout(() => {
-            this.el.nativeElement.childNodes[0].classList.add('is-fluid');
-        });
+export class BulmaContainerFluidDirective implements OnInit {
+    constructor(private el: ElementRef) { }
+
+    ngOnInit(): void {
+        this.el.nativeElement.childNodes[0].classList.add('is-fluid');
     }
 }
 
 @Directive({
     selector: 'bulma-container[fullhd]',
 })
-export class BulmaContainerFullHdDirective {
-    constructor(private el: ElementRef) {
-        setTimeout(() => {
-            this.el.nativeElement.childNodes[0].classList.add('is-fullhd');
-        });
-        
+export class BulmaContainerFullHdDirective implements OnInit {
+    constructor(private el: ElementRef) { }
+
+    ngOnInit(): void {
+        this.el.nativeElement.childNodes[0].classList.add('is-fullhd');
     }
 }
 
 @Directive({
     selector: 'bulma-container[widescreen]',
 })
-export class BulmaContainerWideScreedDirective {
-    constructor(private el: ElementRef) {
-        setTimeout(() => {
-            this.el.nativeElement.childNodes[0].classList.add('is-widescreen');
-        });
-        
+export class BulmaContainerWideScreedDirective implements OnInit  {
+    constructor(private el: ElementRef) { }
+
+    ngOnInit(): void {
+        this.el.nativeElement.childNodes[0].classList.add('is-widescreen');
     }
 }
